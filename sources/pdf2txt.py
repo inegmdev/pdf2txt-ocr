3219a7d98a395c2b934a3302822d5fab16735b90
inegm dev <islam.g.negm@gmail.com>
2023-05-12 22:36:56 +0000

    Add options to control the DPI

--- sources/pdf2txt.py
+++ sources/pdf2txt.py
@@ -1,25 +1,3 @@
-# import os
-# import re
-# from pdf2image import convert_from_path
-
-# def save_txt_page(img_path, txt_path):
-#   page_text = ocr_page(img_path)
-#   with open(txt_path, 'w') as file:
-#     file.write(page_text)
-
-# OUT_IMG_DIR = './tests/pdf2txt/output/sample.pdf.out.images'
-# OUT_TXT_DIR = './tests/pdf2txt/output/sample.pdf.out.text'
-
-# os.makedirs(os.path.join(os.getcwd(),OUT_TXT_DIR), exist_ok=True)
-
-# pdf_to_images('./tests/pdf2txt/input_sample/sample.pdf', OUT_IMG_DIR)
-# pdf_output_all_files = os.listdir(OUT_IMG_DIR)
-# pdf_output_images = list(filter(lambda file: (re.search(r"^[^\.]+\.jpg", file)), pdf_output_all_files)) 
-
-# for page in pdf_output_images:
-#   page_name, _ = os.path.splitext(os.path.basename(OUT_IMG_DIR + "/" + page))
-#   print(f"{OUT_IMG_DIR}/{page}")
-#   save_txt_page (f"{OUT_IMG_DIR}/{page}", f"{OUT_TXT_DIR}/{page_name}.txt")
 import pdf2image
 import pytesseract
 import os
@@ -29,9 +7,13 @@ from tqdm import tqdm
 @click.command()
 @click.option('-i', '--input-dir', 'dir_path', type=click.Path(exists=True), required=True, help='Input directory containing PDF files')
 @click.option('-o', '--output-dir', type=click.Path(), required=True, help='Output directory to store text files')
-def extract_text(dir_path, output_dir):
-    # Initialize the progress bar
-    pbar = tqdm(total=sum([len(files) for r, d, files in os.walk(dir_path)]), unit='file')
+@click.option('-d', '--dpi', type=int, default=100, show_default=True, help='DPI level for converting PDF to images')
+def extract_text(dir_path, output_dir, dpi):
+    # Count the number of PDF files in the input directory
+    num_files = sum([len(files) for r, d, files in os.walk(dir_path) if any(file.endswith('.pdf') for file in files)])
+
+    # Initialize the progress bar for all files
+    all_files_pbar = tqdm(total=num_files, unit='file', leave=False)
 
     # Iterate over the directories and subdirectories in the directory
     for root, dirs, files in os.walk(dir_path):
@@ -42,13 +24,18 @@ def extract_text(dir_path, output_dir):
                 # Get the path to the PDF file
                 pdf_path = os.path.join(root, file_name)
 
-                # Convert the PDF to images
-                images = pdf2image.convert_from_path(pdf_path, dpi=100)
-                print(f'PDF ({file_name}) contains ({len(images)}) pages.')
+                # Convert the PDF to images with the specified DPI level
+                images = pdf2image.convert_from_path(pdf_path, dpi=dpi)
 
                 # Create a directory with the name of the PDF file to store the output text files
                 pdf_output_dir = os.path.join(output_dir, os.path.splitext(file_name)[0])
                 os.makedirs(pdf_output_dir, exist_ok=True)
+
+                # Display the number of pages in the PDF file
+                print(f"PDF {file_name} contains {len(images)} pages.")
+
+                # Initialize the progress bar for the current file
+                file_pbar = tqdm(total=len(images), unit='page')
 
                 # Iterate over the images
                 for j, image in enumerate(images):
@@ -57,13 +44,24 @@ def extract_text(dir_path, output_dir):
                     with open(f"{pdf_output_dir}/Page_{j+1}.txt", "w") as f:
                         f.write(text)
 
-                # Update the progress bar
-                pbar.update(1)
+                    # Update the progress bar for the current file
+                    file_pbar.update(1)
 
-    # Close the progress bar
-    pbar.close()
+                # Close the progress bar for the current file
+                file_pbar.close()
 
-    print("OCR completed successfully.")
+                # Remove the statement about the number of pages in the PDF file
+                print("\033[F\033[K", end="")
+
+                # Update the progress bar for all files
+                all_files_pbar.update(1)
+
+    # Close the progress bar for all files
+    all_files_pbar.close()
+
+    print("pdf2txt completed successfully.")
+    print(f"Input PDF directory   ({dir_path}).")
+    print(f"Text output directory ({output_dir}).")
 
 if __name__ == '__main__':
     extract_text()
